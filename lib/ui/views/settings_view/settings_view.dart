0a8ab7635525e3e0cf361bb52ec6396d3192eba7
Twwan <65574497+Twwan@users.noreply.github.com>
2024-04-20 19:46:43 +0700

    commit fuck it

--- lib/ui/views/settings_view/settings_view.dart
+++ lib/ui/views/settings_view/settings_view.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:provider/provider.dart';
+import 'package:vmeste/ui/theme/colors.dart';
 import 'settings_view_model.dart';
 
 class SettingsView extends StatelessWidget {
@@ -15,7 +16,6 @@ class SettingsView extends StatelessWidget {
       body: SingleChildScrollView(
         padding: const EdgeInsets.all(20),
         child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
             SizedBox(
               width: 120,
@@ -32,27 +32,60 @@ class SettingsView extends StatelessWidget {
                       child: Image.asset('assets/images/person.png', width: 120, height: 120),
                     ),
             ),
-            const SizedBox(height: 16),
-            ElevatedButton(
+            const SizedBox(height: 20),
+            TextButton(
+              style: TextButton.styleFrom(
+                foregroundColor: Colors.white,
+                backgroundColor: AppColors.green,
+                padding: const EdgeInsets.all(16.0),
+                minimumSize: Size(MediaQuery.of(context).size.width * 0.6, 0),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(20.0),
+                ),
+              ),
               onPressed: viewModel.selectImage,
-              child: const Text('Выбрать изображение'),
+              child: Text(
+                'Выбрать изображение',
+                style: Theme.of(context).textTheme.bodyText1,
+              ),
             ),
-            const SizedBox(height: 16),
+            const SizedBox(height: 20),
             TextField(
-              decoration: const InputDecoration(labelText: 'Город'),
+              decoration: InputDecoration(
+                border: const OutlineInputBorder(),
+                labelText: 'Город',
+                labelStyle: TextStyle(color: AppColors.black),
+                focusedBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: AppColors.darkPurple),
+                ),
+              ),
               onChanged: viewModel.updateCity,
             ),
-            const SizedBox(height: 16),
+            const SizedBox(height: 20),
             TextField(
-              decoration: const InputDecoration(labelText: 'Дата рождения (дд.мм.гггг)'),
+              decoration: InputDecoration(
+                border: const OutlineInputBorder(),
+                labelText: 'Дата рождения (дд.мм.гггг)',
+                labelStyle: TextStyle(color: AppColors.black),
+                focusedBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: AppColors.darkPurple),
+                ),
+              ),
               inputFormatters: [FilteringTextInputFormatter.digitsOnly],
               keyboardType: TextInputType.number,
               onChanged: viewModel.updateBirthDate,
             ),
-            const SizedBox(height: 16),
+            const SizedBox(height: 20),
             // Выпадающее меню для выбора пола
             DropdownButtonFormField<Gender>(
-              decoration: const InputDecoration(labelText: 'Пол'),
+              decoration: InputDecoration(
+                border: const OutlineInputBorder(),
+                labelText: 'Пол',
+                labelStyle: TextStyle(color: AppColors.black),
+                focusedBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: AppColors.darkPurple),
+                ),
+              ),
               value: viewModel.gender,
               onChanged: viewModel.updateGender,
               items: const [
@@ -66,10 +99,22 @@ class SettingsView extends StatelessWidget {
                 ),
               ],
             ),
-            const SizedBox(height: 16),
-            ElevatedButton(
+            const SizedBox(height: 20),
+            TextButton(
+              style: TextButton.styleFrom(
+                foregroundColor: Colors.white,
+                backgroundColor: AppColors.darkPurple,
+                padding: const EdgeInsets.all(16.0),
+                minimumSize: Size(MediaQuery.of(context).size.width * 0.8, 0),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(20.0),
+                ),
+              ),
               onPressed: viewModel.saveSettings,
-              child: const Text('Сохранить'),
+              child: Text(
+                'Сохранить',
+                style: Theme.of(context).textTheme.bodyText1,
+              ),
             ),
           ],
         ),
